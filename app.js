ff5068a55882b82ac5b26ef6988983f322ad9aff
xProMDFKx <37838961+xProMDFKx@users.noreply.github.com>
2018-08-27 15:15:40 +0300

    Update app.js

--- app.js
+++ app.js
@@ -11,6 +11,49 @@ const config = require("./config.json");
 // config.token contains the bot's token
 // config.prefix contains the message prefix.
 
+exports.run = (client, message, args) => {
+  let reason = args.slice(1).join(' ');
+  let user = message.mentions.users.first();
+  let modlog = client.channels.find('name', 'mod-log');
+  let muteRole = client.guilds.get(message.guild.id).roles.find('name', 'muted');
+  if (!modlog) return message.reply('I cannot find a mod-log channel').catch(console.error);
+  if (!muteRole) return message.reply('I cannot find a mute role').catch(console.error);
+  if (reason.length < 1) return message.reply('You must supply a reason for the mute.').catch(console.error);
+  if (message.mentions.users.size < 1) return message.reply('You must mention someone to mute them.').catch(console.error);
+  const embed = new Discord.RichEmbed()
+    .setColor(0x00AE86)
+    .setTimestamp()
+    .addField('Action:', 'Un/Mute')
+    .addField('User:', `${user.username}#${user.discriminator}`)
+    .addField('Modrator:', `${message.author.username}#${message.author.discriminator}`);
+
+  if (!message.guild.member(client.user).hasPermission('MANAGE_ROLES_OR_PERMISSIONS')) return message.reply('I do not have the correct permissions.').catch(console.error);
+
+  if (message.guild.member(user).roles.has(muteRole.id)) {
+    message.guild.member(user).removeRole(muteRole).then(() => {
+      client.channels.get(modlog.id).sendEmbed(embed).catch(console.error);
+    });
+  } else {
+    message.guild.member(user).addRole(muteRole).then(() => {
+      client.channels.get(modlog.id).sendEmbed(embed).catch(console.error);
+    });
+  }
+
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'mute',
+  description: 'mutes or unmutes a mentioned user',
+  usage: 'un/mute [mention]'
+};
+
 client.on("ready", () => {
   // This event will run if the bot starts, and logs in, successfully.
   console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
